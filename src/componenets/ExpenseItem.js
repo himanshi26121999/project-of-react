5bbc189bae5a56adf0901dd197669522217e86bd
himanshi26121999 <abhayrajc55@gmail.com>
2025-06-15 16:06:15 +0530

    JSX using JS

--- src/componenets/ExpenseItem.js
+++ src/componenets/ExpenseItem.js
@@ -1,11 +1,16 @@
 import "./ExpenseItem.css";
 function ExpenseItem(){
+    const expenseDate=new Date(2020,8,15).toISOString();
+    const expenseTitle="Book";
+    const expensePrice=20;
+    const expenseLOcation="Bangalore";
     return(
         <div className="expense-item">
-        <div>15 Augusgitt 2020</div>
+        <div>{expenseDate}</div>
+        <div className="expense-item__location">{expenseLOcation}</div>
         <div className="expense-item__description">
-            <h2>Book</h2>
-            <div className="expense-item__price>">$10</div>
+            <h2>{expenseTitle}</h2>
+            <div className="expense-item__price>">${expensePrice}</div>
         </div>
         </div>
     )
