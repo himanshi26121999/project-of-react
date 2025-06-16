816a881877d3ea4005b4887f37e8f43aa6e941b5
himanshi26121999 <abhayrajc55@gmail.com>
2025-06-16 13:25:42 +0530

    react using map

--- src/App.js
+++ src/App.js
@@ -1,33 +1,25 @@
 import ExpenseItem from "./componenets/ExpenseItem";
 function App() {
+  const expenses=[
+    {id:"1",date:new Date(2023,7,15),titel:"Insurance",location:"Delhi",price:"100"},
+    {id:"2",date:new Date(2023,8,25),titel:"Book",location:"Goa",price:"200"},
+  {id:"3",date:new Date(2023,9,5),titel:"Spoon",location:"Hyderabad",price:"10"},
+  {id:"4",date:new Date(2023,10,11),titel:"Food",location:"Bangalore",price:"110"},
+  {id:"5",date:new Date(2023,11,15),titel:"Mango",location:"Koichi",price:"900"},
+  ]
   return (
     <div>
       <h1>Let's get started</h1>
       <p>I am learning React</p>
-      <ExpenseItem 
-      date={new Date(2023,1,1)}
-      title="Book"
-      price="10"
-      location="delhi">
-      </ExpenseItem>
-      <ExpenseItem 
-      date={new Date(2023,1,14)}
-      title="Travelling"
-      price="112"
-      location="MP">
-      </ExpenseItem>
-      <ExpenseItem 
-      date={new Date(2023,1,12)}
-      title="Paneer"
-      price="100"
-      location="Locknow">
-      </ExpenseItem>
-      <ExpenseItem 
-      date={new Date(2023,12,1)}
-      title="Swimming"
-      price="100"
-      location="Goa">
-      </ExpenseItem>
+      {expenses.map((expense,index)=>{
+        return(
+          <ExpenseItem key={expense.id}
+                      date={expense.date}
+                      title={expense.titel}
+                      location={expense.location}
+                      price={expense.price}></ExpenseItem>
+        )
+      })}
     </div>
   );
 }
