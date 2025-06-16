031aba05d5830cc2b429a0ef40eac40a0e2ee47b
himanshi26121999 <abhayrajc55@gmail.com>
2025-06-16 14:46:19 +0530

    Giving Calender look

--- src/componenets/ExpenseItem.js
+++ src/componenets/ExpenseItem.js
@@ -1,16 +1,20 @@
-import "./ExpenseItem.css";
-function ExpenseItem(){
-    const expenseDate=new Date(2020,8,15).toISOString();
-    const expenseTitle="Book";
-    const expensePrice=20;
-    const expenseLOcation="Bangalore";
+ import "./ExpenseItem.css";
+ const months=["January","February","March","April","May","June","July","August","September","October","November","December"];
+function ExpenseItem(props){
+    const month=months[props.date.getMonth()];
+    const year=props.date.getFullYear();
+    const day=props.date.getDate();
     return(
         <div className="expense-item">
-        <div>{expenseDate}</div>
-        <div className="expense-item__location">{expenseLOcation}</div>
+        <div>
+            <div>{month}</div>
+            <div>{year}</div>
+            <div>{day}</div>
+        </div>
+        <div className="expense-item__location">{props.location}</div>
         <div className="expense-item__description">
-            <h2>{expenseTitle}</h2>
-            <div className="expense-item__price>">${expensePrice}</div>
+            <h2>{props.title}</h2>
+            <div className="expense-item__price>">${props.price}</div>
         </div>
         </div>
     )
