a8b8c14f36e6b95edb046e4d740cf94a1d01ac1f
himanshi26121999 <abhayrajc55@gmail.com>
2025-06-16 11:57:56 +0530

    Props object

--- src/App.js
+++ src/App.js
@@ -1,8 +1,33 @@
+import ExpenseItem from "./componenets/ExpenseItem";
 function App() {
   return (
     <div>
       <h1>Let's get started</h1>
       <p>I am learning React</p>
+      <ExpenseItem 
+      date={new Date(2023,1,1)}
+      title="Book"
+      price="10"
+      location="delhi">
+      </ExpenseItem>
+      <ExpenseItem 
+      date={new Date(2023,1,14)}
+      title="Travelling"
+      price="112"
+      location="MP">
+      </ExpenseItem>
+      <ExpenseItem 
+      date={new Date(2023,1,12)}
+      title="Paneer"
+      price="100"
+      location="Locknow">
+      </ExpenseItem>
+      <ExpenseItem 
+      date={new Date(2023,12,1)}
+      title="Swimming"
+      price="100"
+      location="Goa">
+      </ExpenseItem>
     </div>
   );
 }
