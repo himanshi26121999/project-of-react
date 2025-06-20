b6a4209c50ad249408890192894645d4a20f56aa
himanshi26121999 <abhayrajc55@gmail.com>
2025-06-20 16:06:35 +0530

    add event listener

--- src/componenets/ExpenseDiv/ExpenseItem.js
+++ src/componenets/ExpenseDiv/ExpenseItem.js
@@ -2,6 +2,9 @@
  import ExpenseDate from "./ExpenseDate";
  import Card from "../WrapperDiv/Card"
 function ExpenseItem(props){
+    function buttonOnClickHandeler(event){
+        console.log("Button clicked");
+    }
 
     return(
       <Card className="expense-item"> 
@@ -11,6 +14,7 @@ function ExpenseItem(props){
             <h2>{props.title}</h2>
             <div className="expense-item__price>">${props.price}</div>
         </div>
+        <button onClick={buttonOnClickHandeler}>Change Title</button>
         </Card>
     )
 }
