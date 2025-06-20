b1d31234359d1347e034331ebe4fc3d0e34831a6
himanshi26121999 <abhayrajc55@gmail.com>
2025-06-20 16:58:16 +0530

    useStateHook

--- src/componenets/ExpenseDiv/ExpenseItem.js
+++ src/componenets/ExpenseDiv/ExpenseItem.js
@@ -1,9 +1,11 @@
+ import React,{useState} from "react";
  import "./ExpenseItem.css";
  import ExpenseDate from "./ExpenseDate";
  import Card from "../WrapperDiv/Card"
 function ExpenseItem(props){
+    const [title,steTitle]=useState(props.title);
     function buttonOnClickHandeler(event){
-        console.log("Button clicked");
+        steTitle("New Title");
     }
 
     return(
@@ -11,7 +13,7 @@ function ExpenseItem(props){
       <ExpenseDate calenderDate={props.date}/>
         <div className="expense-item__location">{props.location}</div>
         <div className="expense-item__description">
-            <h2>{props.title}</h2>
+            <h2>{title}</h2>
             <div className="expense-item__price>">${props.price}</div>
         </div>
         <button onClick={buttonOnClickHandeler}>Change Title</button>
